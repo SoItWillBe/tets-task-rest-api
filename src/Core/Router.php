cf133e91c5d52ac09a69b3be90f3886e0b0ec685
David <work.vladshamrai@gmail.com>
2025-10-05 17:35:24 +0300

    Added identification and placeholder replacement.

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -12,6 +12,12 @@ class Router implements RouterInterface
 
     public function registerRoute(string $method, string $url, array $handler): Router
     {
+        $urlWithReplacedPlaceholder = $this->matchRoute($url, $_SERVER['REQUEST_URI']);
+
+        if (null !== $urlWithReplacedPlaceholder) {
+            $url = $urlWithReplacedPlaceholder;
+        }
+
         if (isset($this->routerContainer[$method][$url])) {
             throw new \Exception(sprintf('Route [%s] %s already registered!', $method, $url));
         }
@@ -38,5 +44,17 @@ class Router implements RouterInterface
     public function getRoute($method, $uri): ?array
     {
         return $this->routerContainer[$method][$uri] ?? null;
+    }
+
+    private function matchRoute(string $pattern, string $uri): ?string
+    {
+        $regex = preg_replace('#:([\w]+)#', '(?P<$1>[^/]+)', $pattern);
+        $regex = "#^" . $regex . "$#";
+
+        if (preg_match($regex, $uri, $matches)) {
+            return str_replace($pattern, $uri, $pattern);
+        }
+
+        return null;
     }
 }
