89487a69c6707c5b840fd14b97bacbaa51b7c993
David <work.vladshamrai@gmail.com>
2025-10-05 21:38:09 +0300

    UserQueryManager created; In development.

--- src/Core/QueryManagers/UsersQueryManager.php
+++ src/Core/QueryManagers/UsersQueryManager.php
@@ -1,1 +1,24 @@
 <?php
+
+namespace App\Core\QueryManagers;
+
+use App\Core\QueryManager;
+
+class UsersQueryManager extends QueryManager
+{
+    protected \PDO $pdo;
+
+    protected string $table;
+
+    public function __construct(\PDO $pdo)
+    {
+        $this->pdo = $pdo;
+        $this->table = 'users';
+    }
+
+    public function all()
+    {
+        return $this->select()->execute();
+    }
+
+}
