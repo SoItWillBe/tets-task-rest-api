f4c382d08dddd6354583011d3e0a22a860acdc08
Salah ad-Din <work.vladshamrai@gmail.com>
2025-10-08 13:46:27 +0300

    Minimal container with user data.

--- src/Core/Container/UserContainer.php
+++ src/Core/Container/UserContainer.php
@@ -1,1 +1,19 @@
 <?php
+
+namespace App\Core\Container;
+
+class UserContainer
+{
+
+    private static $id;
+
+    public static function setUserId(int $id)
+    {
+        self::$id = $id;
+    }
+
+    public static function getUserId(): ?int
+    {
+        return self::$id ?? null;
+    }
+}
