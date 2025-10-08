69287d570ed33b6018f27543fc7cd0e05eeef241
Salah ad-Din <work.vladshamrai@gmail.com>
2025-10-08 13:47:19 +0300

    unified response

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -2,15 +2,32 @@
 
 namespace App\Controllers;
 
+use App\Core\Http\Request;
+use App\Core\QueryManagers\UsersQueryManager;
 use App\Interfaces\ControllerInterface;
+use App\Services\User\UserService;
 
+/**
+ * Main controller. Unifies response.
+ */
 abstract class Controller implements ControllerInterface
 {
-    public function jsonResponse(array $message, int $statusCode = 200): void
+
+    public function __construct()
+    {}
+
+    public function jsonResponse(array $message, int $statusCode = 200, array $headers = null): void
     {
         $message = json_encode($message, JSON_UNESCAPED_UNICODE);
         header('Content-Type: application/json');
+        if (!empty($headers)) {
+            foreach ($headers as $header)
+            {
+                header($header);
+            }
+        }
         http_response_code($statusCode);
         echo $message;
+        exit();
     }
 }
