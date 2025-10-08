a1b22f668074a1ee0c156e59b7b6d787683f8bca
Salah ad-Din <work.vladshamrai@gmail.com>
2025-10-08 13:45:48 +0300

    Minimal Middleware layout implemented. This steps cheks if user must be logged in, does user has token if auth needed. Then we queries users id and if users token is similar to token we returns id.

--- src/Core/Http/Middleware.php
+++ src/Core/Http/Middleware.php
@@ -2,6 +2,33 @@
 
 namespace App\Core\Http;
 
-class Middleware
+final class Middleware
 {
+    public static function checkAuth(bool $handler, array $headers, \PDO $pdo): ?int
+    {
+        // auth is not provided for handler, so nothing to check.
+        if (false === $handler)
+        {
+            return null;
+        }
+
+        // return false if auth needed but header is not found
+        if (!isset($headers['Authorization']))
+        {
+            return -1;
+        }
+
+        $token = str_replace('Bearer ', '', $headers['Authorization']);
+
+        $stmt = $pdo->prepare("SELECT id FROM users_token WHERE token = :token");
+        $stmt->execute(['token' => $token]);
+        $user = $stmt->fetch();
+
+        if (false !== $user)
+        {
+            return $user['id'];
+        }
+
+        return -1;
+    }
 }
