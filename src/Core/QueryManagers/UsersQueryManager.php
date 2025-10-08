71e0a099fb7800c3986ffcd123edd27fbb51b21a
Salah ad-Din <work.vladshamrai@gmail.com>
2025-10-08 14:09:01 +0300

    added methods: query, queryLike, create, updateUser. Remove method implemented.

--- src/Core/QueryManagers/UsersQueryManager.php
+++ src/Core/QueryManagers/UsersQueryManager.php
@@ -2,8 +2,6 @@
 
 namespace App\Core\QueryManagers;
 
-use App\Core\QueryManager;
-
 class UsersQueryManager extends QueryManager
 {
     protected \PDO $pdo;
@@ -16,9 +14,36 @@ class UsersQueryManager extends QueryManager
         $this->table = 'users';
     }
 
-    public function all()
+    public function query(array $rows = null, array $where = null): ?array
     {
-        return $this->select()->execute();
+        $select = $this->select($rows);
+        if (null !== $where) {
+            $select->where($where);
+        }
+        return $select->execute();
     }
 
+    public function queryLike(array $rows = null, array $like = null): ?array
+    {
+        return $this->select($rows)
+            ->like($like)
+            ->execute();
+    }
+
+    public function create(array $payload): ?array
+    {
+        return $this->insert($payload) ?
+            ['id' => $this->pdo->lastInsertId()] :
+            null;
+    }
+
+    public function updateUser(int $id, array $payload): ?bool
+    {
+        return $this->update($payload, ['id' => $id]);
+    }
+
+    public function remove(int $id): bool
+    {
+        return $this->delete(['user_id' => $id]);
+    }
 }
