03be0eedc509ee67b53c19ae9672a6e34c21dbe8
Salah ad-Din <work.vladshamrai@gmail.com>
2025-10-08 14:06:10 +0300

    constants added for response statuses; ResponseMessage added; CS Fix.

--- src/Controllers/User/UserController.php
+++ src/Controllers/User/UserController.php
@@ -3,38 +3,72 @@
 namespace App\Controllers\User;
 
 use App\Controllers\Controller;
+use App\Core\Http\Request;
 use App\Core\QueryManagers\UsersQueryManager;
+use App\Helpers\ResponseMessage;
+use App\Helpers\ResponseStatusesEnums;
 use App\Services\User\UserService;
 
 class UserController extends Controller
 {
 
+    const SUCCESS = ResponseStatusesEnums::Success;
+
+    const ERROR = ResponseStatusesEnums::Error;
+
     private UserService $service;
 
-    public function __construct(\PDO $pdo)
+    private Request $request;
+
+    public function __construct(\PDO $pdo, Request $request)
     {
+        parent::__construct();
         $this->service = new UserService(
-            new UsersQueryManager($pdo)
+            new UsersQueryManager($pdo),
+            $request
         );
+        $this->request = $request;
     }
 
     public function index()
     {
         $this->jsonResponse(
-            ['message' => $this->service->getAllUsers()]
+            ResponseMessage::response(self::SUCCESS, $this->service->getAllUsers())
         );
     }
 
     public function show($id)
     {
+        // fetching user by if
+        $user = $this->service->getUserById($id);
+
+        // returns error if user not found
+        $status = empty($user) ? self::ERROR : self::SUCCESS;
+
+        // set response code depending on previous result
+        $code = empty($user) ? 404 : 200;
+
         $this->jsonResponse(
-            ['message' => $this->service->getUserById($id)]
+            ResponseMessage::response($status, $user, $code)
         );
     }
 
+    public function update($id)
+    {
+        $update = $this->service->updateUser($id, $this->request->json);
+
+        $this->jsonResponse(
+            ResponseMessage::response($update['status'], $update['message'], $update['code'])
+        );
+    }
+    
     public function delete($id)
     {
-        $this->jsonResponse(['message' => 'delete success']);
+        $delete = $this->service->deleteUser($id);
+
+        $this->jsonResponse(
+            ResponseMessage::response($delete['status'], $delete['message'], $delete['code'])
+        );
     }
 
 }
