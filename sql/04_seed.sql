9abb96ae426a8de7c81394f612e45924ad65580a
Salah ad-Din <work.vladshamrai@gmail.com>
2025-10-08 17:19:27 +0300

    CS Fix

--- sql/04_seed.sql
+++ sql/04_seed.sql
@@ -1,9 +1,9 @@
-    INSERT INTO users (email, address, age, password) VALUES ('user@example.com','Kyiv, Obolon 1',30,'$2y$10$YUTLQWnOd77zdNEq8aTyf.JfpSzxbwV4IYUaxKeuVG23mjAlXWV3u');
-    INSERT INTO users (email, address, age, password) VALUES ('work@example.com','Svobody Avenue, 28', 32,'$2y$10$YUTLQWnOd77zdNEq8aTyf.JfpSzxbwV4IYUaxKeuVG23mjAlXWV3u');
-    INSERT INTO users (email, address, age, password) VALUES ('home@example.com','Lviv, Svobody Avenue 3', 25,'$2y$10$YUTLQWnOd77zdNEq8aTyf.JfpSzxbwV4IYUaxKeuVG23mjAlXWV3u');
-    INSERT INTO users (email, address, age, password) VALUES ('john@example.com','Soborna 2',29,'$2y$10$YUTLQWnOd77zdNEq8aTyf.JfpSzxbwV4IYUaxKeuVG23mjAlXWV3u');
-    INSERT INTO users (email, address, age, password) VALUES ('doe@example.com','Kyiv',22,'$2y$10$YUTLQWnOd77zdNEq8aTyf.JfpSzxbwV4IYUaxKeuVG23mjAlXWV3u');
-    INSERT INTO users (email, address, age, password) VALUES ('dwight.schrute@example.com','1725 Slough Avenue',22,'$2y$10$YUTLQWnOd77zdNEq8aTyf.JfpSzxbwV4IYUaxKeuVG23mjAlXWV3u');
-    INSERT INTO users (email, address, age, password) VALUES ('jim.halpert@example.com','Scranton, 1725 Slough Avenue',22,'$2y$10$YUTLQWnOd77zdNEq8aTyf.JfpSzxbwV4IYUaxKeuVG23mjAlXWV3u');
+INSERT INTO users (email, address, age, password) VALUES ('user@example.com','Kyiv, Obolon 1',30,'$2y$10$YUTLQWnOd77zdNEq8aTyf.JfpSzxbwV4IYUaxKeuVG23mjAlXWV3u');
+INSERT INTO users (email, address, age, password) VALUES ('work@example.com','Svobody Avenue, 28', 32,'$2y$10$YUTLQWnOd77zdNEq8aTyf.JfpSzxbwV4IYUaxKeuVG23mjAlXWV3u');
+INSERT INTO users (email, address, age, password) VALUES ('home@example.com','Lviv, Svobody Avenue 3', 25,'$2y$10$YUTLQWnOd77zdNEq8aTyf.JfpSzxbwV4IYUaxKeuVG23mjAlXWV3u');
+INSERT INTO users (email, address, age, password) VALUES ('john@example.com','Soborna 2',29,'$2y$10$YUTLQWnOd77zdNEq8aTyf.JfpSzxbwV4IYUaxKeuVG23mjAlXWV3u');
+INSERT INTO users (email, address, age, password) VALUES ('doe@example.com','Kyiv',22,'$2y$10$YUTLQWnOd77zdNEq8aTyf.JfpSzxbwV4IYUaxKeuVG23mjAlXWV3u');
+INSERT INTO users (email, address, age, password) VALUES ('dwight.schrute@example.com','1725 Slough Avenue',22,'$2y$10$YUTLQWnOd77zdNEq8aTyf.JfpSzxbwV4IYUaxKeuVG23mjAlXWV3u');
+INSERT INTO users (email, address, age, password) VALUES ('jim.halpert@example.com','Scranton, 1725 Slough Avenue',22,'$2y$10$YUTLQWnOd77zdNEq8aTyf.JfpSzxbwV4IYUaxKeuVG23mjAlXWV3u');
 
-    INSERT INTO users_token (user_id, token) VALUES ('4','d39e201ba4621bcf43589023fe79502e31373539383533313234');
+INSERT INTO users_token (user_id, token) VALUES ('4','d39e201ba4621bcf43589023fe79502e31373539383533313234');
