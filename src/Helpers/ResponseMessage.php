c8a269c3c9a59d73e8abdf2dad9361c873c2fe82
Salah ad-Din <work.vladshamrai@gmail.com>
2025-10-08 12:45:53 +0300

    Response message unifier added

--- src/Helpers/ResponseMessage.php
+++ src/Helpers/ResponseMessage.php
@@ -1,3 +1,14 @@
 <?php
 
 namespace App\Helpers;
+
+class ResponseMessage
+{
+    public static function response(ResponseStatusesEnums $status, mixed $message): array
+    {
+        return [
+            'status' => $status,
+            'message' => $message,
+        ];
+    }
+}
