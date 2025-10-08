f07ba732aff5dd4996ed9f8939e5c9d32b6d1154
Salah ad-Din <work.vladshamrai@gmail.com>
2025-10-08 17:26:20 +0300

    validate method temporarely removed

--- src/Helpers/Auth/HashHelper.php
+++ src/Helpers/Auth/HashHelper.php
@@ -11,9 +11,4 @@ class HashHelper
     {
         return password_hash($password, PASSWORD_DEFAULT);
     }
-
-    public static function validate(string $hash, string $payload): bool
-    {
-        return password_verify($payload, $hash);
-    }
 }
