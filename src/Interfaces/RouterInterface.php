d0da65acf5dd76e19c12e54a57ef5a772700fc85
Salah ad-Din <work.vladshamrai@gmail.com>
2025-10-08 13:18:05 +0300

    Interface fixed according to router needs; Method is now enum instead string; uri replaced from getRoute

--- src/Interfaces/RouterInterface.php
+++ src/Interfaces/RouterInterface.php
@@ -3,9 +3,10 @@
 namespace App\Interfaces;
 
 use App\Core\Router;
+use App\Helpers\Http\Method;
 
 interface RouterInterface {
-    public function registerRoute(string $method, string $url, array $handler): Router;
+    public function registerRoute(Method $method, string $url, array $handler): Router;
 
-    public function getRoute($method, $uri): ?array;
+    public function getRoute($method): ?array;
 }
