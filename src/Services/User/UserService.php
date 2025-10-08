0f9336264020557ec4791ce447e594f4a3593f04
Salah ad-Din <work.vladshamrai@gmail.com>
2025-10-08 13:13:11 +0300

    Added consts for Response enums; returns formatted; Added comments; CS Fix.

--- src/Services/User/UserService.php
+++ src/Services/User/UserService.php
@@ -2,35 +2,102 @@
 
 namespace App\Services\User;
 
+use App\Core\Container\UserContainer;
+use App\Core\Http\Request;
+use App\Helpers\Auth\HashHelper;
+use App\Helpers\ResponseStatusesEnums;
 use App\Interfaces\QueryManagerInterface;
 
 class UserService
 {
 
-    private $queryManager;
+    private QueryManagerInterface $queryManager;
 
-    public function __construct(QueryManagerInterface $queryManager)
+    private Request $request;
+
+    public function __construct(
+        QueryManagerInterface $queryManager,
+        Request $request
+    )
     {
         $this->queryManager = $queryManager;
+        $this->request = $request;
     }
 
     public function getAllUsers(): array
     {
-        return $this->queryManager->all();
+        $like = (!empty($this->request->get)) ?
+            $this->request->get : null;
+
+        return ($like !== null ) ?
+            $this->queryManager->queryLike(['id', 'email', 'age', 'address', 'created_at'], $like):
+            $this->queryManager->query(['id', 'email', 'age', 'address', 'created_at']);
     }
 
-    public function getUserById($id): array
+    public function getUserById($id): ?array
     {
-        return $this->queryManager
-            ->select(['id', 'email'])
-            ->where(['id' => $id])
-            ->execute();
+        return $this->queryManager->query(
+            ['users.id', 'email', 'age', 'address', 'created_at'],
+            ['id' => $id]
+        );
     }
 
-    public function deleteUser($id): void
+    public function updateUser($id, $request): array
     {
-        $this->queryManager
-            ->delete(['id' => $id])
-            ->execute();
+        if (empty($request)) {
+            return [
+                'status' => ResponseStatusesEnums::Error,
+                'message' => 'data is invalid',
+                'code' => 400
+            ];
+        }
+
+        if (UserContainer::getUserId() !== $id) {
+            return [
+                'status' => ResponseStatusesEnums::Error,
+                'message' => 'you are not allowed to edit this user',
+                'code' => 401
+            ];
+        }
+
+        if (isset($request['email'])) {
+            $payload['email'] = $request['email'];
+        }
+
+        if (isset($request)) {
+            $payload['password'] = HashHelper::hashPassword($request['password']);;
+        }
+
+        return ($this->queryManager->updateUser($id, $payload)) ?
+            [
+                'status' => ResponseStatusesEnums::Success,
+                'message' => 'user updated successfully',
+                'code' => 200
+            ] : [
+                'status' => ResponseStatusesEnums::Error,
+                'message' => 'error while updating user',
+                'code' => 500
+            ];
+    }
+
+    public function deleteUser(int $id): array
+    {
+        if (UserContainer::getUserId() !== $id) {
+            return [
+                'status' => ResponseStatusesEnums::Error,
+                'message' => 'you are not allowed to delete this user',
+                'code' => 401
+            ];
+        }
+        return ($this->queryManager->remove($id)) ?
+            [
+                'status' => ResponseStatusesEnums::Success,
+                'message' => 'user deleted successfully',
+                'code' => 200
+            ] : [
+                'status' => ResponseStatusesEnums::Error,
+                'message' => 'error while deleting user',
+                'code' => 500
+            ];
     }
 }
